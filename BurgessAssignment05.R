ed5a230cec04dd2eb5301a69bdfe73afacaed62c
Kenny Burgess <kwb223@lehigh.edu>
2019-11-14 21:48:28 -0500

    I did HW 5

---
+++ BurgessAssignment05.R
@@ -0,0 +1,10 @@
+############################################################
+# R script to accompany Assignment 05 
+# Written by Kenny Burgess                                 #
+############################################################
+
+DF <- data.table::fread("FEDFUNDS.csv")
+data.table::fread("FEDFUNDS.csv")
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Assignment05
+HW is in assignment05 Rscript 
